50695c64e5e21de5ba24efa2acb648d94dc81fe1
lameck <lameck2@hotmail.com>
2018-06-16 11:17:18 +0000

    route

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const PORT = 3000;
 
 const server = express();
 
+server.get('/', (req, res)=> {
+    res.send("My first route using Express.js")
+})
+
 server.listen(PORT, ()=> {
     console.log(`Server started on ${PORT}`);
 })
