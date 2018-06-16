e2a74a62f8331ea3a92e0eae993f3a597ab7625b
lameck <lameck2@hotmail.com>
2018-06-16 11:48:03 +0000

    Json API

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 import express from 'express';
+import students from './data/students.json';
 
 const PORT = 3000;
-
 const server = express();
+const buildUrl = (version, path) => `/api/${version}/${path}`;
 
-server.get('/', (req, res)=> {
-    res.send("My first route using Express.js")
+server.get(buildUrl('v1','students'),(req, res)=> {
+    res.json(students);
 })
 
 server.listen(PORT, ()=> {
