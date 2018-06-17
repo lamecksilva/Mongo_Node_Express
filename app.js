0aa094af8023453a91a535ff1e0be5f41cf2e0b3
lameck <lameck2@hotmail.com>
2018-06-17 11:31:31 +0000

    Static files Express.js

--- app.js
+++ app.js
@@ -4,17 +4,24 @@ import StudentRoute from './routes/StudentRoute';
 import students from './data/students.json';
 import _ from 'lodash';
 import bodyParser from 'body-parser';
+import path from 'path';
 
 const PORT = 3000;
 const server = express();
 
 server.use(morgan('tiny'));
 server.use(bodyParser.json());
+server.use('/static', express.static('public'));
 
 const buildUrl = (version, path) => `/api/${version}/${path}`;
 const STUDENTS_BASE_URL = buildUrl('v1','students');
 
 server.use(STUDENTS_BASE_URL, StudentRoute);
+
+server.get('/download/images/:imageName', (req, res) => {
+    res.download(path.join('public', 'images', req.params.imageName));
+});
+
 
 server.get('/route-handlers', (req, res, next) => {
     res.send("Learning route handlers is cool");
